7522f5680b6fb509d489e40ed1a2bd19e2a3d93f
Gaurav Lall <lallgaurav538@gmail.com>
2024-01-21 12:05:55 +0530

    Added my project

--- demo/src/main/java/com/example/demo/HelloController.java
+++ demo/src/main/java/com/example/demo/HelloController.java
@@ -1,12 +1,19 @@
 package com.example.demo;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:3000")
 public class HelloController {
 	
 	@Autowired 
@@ -45,9 +52,41 @@ public class HelloController {
     	return emp;
     }
     
-    @PostMapping("/addemployees")
-    void addEmployee(@RequestBody EmployeeMaster e) {
-    	employeeRepository.save(e);
+    @PostMapping("/employees")
+    public ResponseEntity addEmployee(@RequestBody EmployeeMaster e) throws URISyntaxException{
+    	
+    	EmployeeMaster savedClient = employeeRepository.save(e);
+        return ResponseEntity.created(new URI("/addemployees/" + savedClient.getId())).body(savedClient);
+    	//employeeRepository.save(e);
+    }
+    
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteClient(@PathVariable Integer id) {
+    	employeeRepository.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
+    
+ // get employee by id rest api
+    @GetMapping("/employees/{id}")
+    public ResponseEntity <EmployeeMaster> getEmployeeById(@PathVariable Integer id) {
+    	EmployeeMaster employee = employeeRepository.findById(id).get();
+        return ResponseEntity.ok(employee);
+    }
+    
+    
+    @PutMapping("/employees/{id}")
+    public ResponseEntity < EmployeeMaster > updateEmployee(@PathVariable Integer id, @RequestBody EmployeeMaster employeeDetails) {
+    	EmployeeMaster employee = employeeRepository.findById(id).get();
+
+    	employee.setName(employeeDetails.getName());
+		/*
+		 * employee.setFirstName(employeeDetails.getFirstName());
+		 * employee.setLastName(employeeDetails.getLastName());
+		 * employee.setEmailId(employeeDetails.getEmailId());
+		 */
+
+    	EmployeeMaster updatedEmployee = employeeRepository.save(employee);
+        return ResponseEntity.ok(updatedEmployee);
     }
 
 }
